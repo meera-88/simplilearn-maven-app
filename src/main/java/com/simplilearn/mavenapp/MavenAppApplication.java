ac4f03a56b9f08b97888831b96c5060e4ad6b13a
meera-88 <meera.m.biradar@gmail.com>
2020-11-30 09:44:01 +0530

    dummy spring boot maven app

---
+++ src/main/java/com/simplilearn/mavenapp/MavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.mavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/mavenapp/MavenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.mavenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
